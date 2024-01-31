78d647e5bba32cd546bc1406cea202875319b107
Sergey Natalenko <sergey.natalenko@mail.ru>
2024-01-31 05:19:40 +0300

    Update docker-compose.yml

--- vk_parser/parsers/vk/arguments.py
+++ vk_parser/parsers/vk/arguments.py
@@ -34,10 +34,6 @@ group.add_argument(
     choices=LogFormat.choices(),
 )
 
-group = parser.add_argument_group("API options")
-group.add_argument("--api-address", default="127.0.0.1")
-group.add_argument("--api-port", type=int, default=8000)
-
 group = parser.add_argument_group("VK API tokens")
 group.add_argument("--vk-api-secure-key", type=str, required=True)
 group.add_argument("--vk-api-service-token", type=str, required=True)
