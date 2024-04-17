83972bd6167ceaa2a3cbb87558406a66b032e67a
Yzurgzd <galievr469@gmail.com>
2024-04-17 16:26:20 +0300

    Add account limits

--- vk_parser/db/models/vk_user_messanger.py
+++ vk_parser/db/models/vk_user_messanger.py
@@ -9,16 +9,29 @@ MAX_SUCCESSFUL_MESSAGES = 20
 class SendAccounts(TimestampMixin, Base):
     """Аккаунты с которых будет производиться рассылка"""
 
-    id: Mapped[int] = mapped_column(BigInteger, primary_key=True)
+    id: Mapped[int] = mapped_column(
+        BigInteger,
+        primary_key=True,
+    )
     user_id: Mapped[int] = mapped_column(
         BigInteger,
         ForeignKey("auth_user.id", ondelete="CASCADE"),
         nullable=False,
         index=True,
     )
-    login: Mapped[str] = mapped_column(String(1024), nullable=False)
-    password: Mapped[str] = mapped_column(String(1024), nullable=False)
-    secret_token: Mapped[str] = mapped_column(String(1024), nullable=False)
+    login: Mapped[str] = mapped_column(
+        String(1024),
+        nullable=False,
+        unique=True,
+    )
+    password: Mapped[str] = mapped_column(
+        String(1024),
+        nullable=False,
+    )
+    secret_token: Mapped[str] = mapped_column(
+        String(1024),
+        nullable=False,
+    )
     user_link: Mapped[str | None] = mapped_column(
         String(1024),
         nullable=True,
@@ -45,7 +58,10 @@ class SendAccounts(TimestampMixin, Base):
 class Messages(TimestampMixin, Base):
     """Сообщения для массовой рассылки пользователям"""
 
-    id: Mapped[int] = mapped_column(BigInteger, primary_key=True)
+    id: Mapped[int] = mapped_column(
+        BigInteger,
+        primary_key=True,
+    )
     user_id: Mapped[int] = mapped_column(
         BigInteger,
         ForeignKey("auth_user.id", ondelete="CASCADE"),
