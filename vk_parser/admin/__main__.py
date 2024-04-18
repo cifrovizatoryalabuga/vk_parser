dae5b6b6b4dfc16d2fa37167b89e28b7abf29e34
Yzurgzd <galievr469@gmail.com>
2024-04-18 14:49:14 +0300

    Change Celery to APScheduler

--- vk_parser/admin/__main__.py
+++ vk_parser/admin/__main__.py
@@ -2,10 +2,12 @@ import logging
 
 from aiomisc import Service, entrypoint
 from aiomisc_log import basic_config
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 from vk_parser.admin.arguments import parser
 from vk_parser.admin.deps import config_deps
 from vk_parser.admin.service import Admin
+from vk_parser.tasks.resetting_send_accounts_task import reset_send_accounts
 
 log = logging.getLogger(__name__)
 
@@ -14,6 +16,9 @@ def main() -> None:
     args = parser.parse_args()
     basic_config(level=args.log_level, log_format=args.log_format)
     config_deps(args)
+
+    scheduler = AsyncIOScheduler()
+    scheduler.add_job(reset_send_accounts, "interval", minutes=2)
 
     services: list[Service] = [
         Admin(
@@ -29,6 +34,7 @@ def main() -> None:
         pool_size=args.pool_size,
         debug=args.debug,
     ) as loop:
+        scheduler.start()
         loop.run_forever()
 
 
