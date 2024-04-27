aff1af9f710398a7aeace04cfebbc2332ba19d0c
Yzurgzd <galievr469@gmail.com>
2024-04-27 10:38:32 +0300

    Fix parser request timout exc

--- vk_parser/admin/handlers/parser_request_create.py
+++ vk_parser/admin/handlers/parser_request_create.py
@@ -1,40 +1,40 @@
 from aiohttp.web import Response, View
-from aiohttp.web_exceptions import HTTPInternalServerError
+from aiohttp.web_exceptions import HTTPInternalServerError, HTTPRequestTimeout
 from aiomisc import timeout
 
 from vk_parser.admin.handlers.base import CreateMixin, DependenciesMixin
 from vk_parser.generals.enums import RequestStatus
-from vk_parser.generals.models.parser_request import (
-    ParsePostsVkForm,
-    SimpleVkForm,
-)
+from vk_parser.generals.models.parser_request import ParsePostsVkForm, SimpleVkForm
 from vk_parser.utils.http import fast_json_response
 
 
 class ParserRequestCreateHandler(View, DependenciesMixin, CreateMixin):
     @timeout(5)
     async def post(self) -> Response:
-        form = await self._parse_json(
-            schemas=[ParsePostsVkForm, SimpleVkForm],
-        )
-
-        parser_request = await self.parser_request_storage.create(
-            input_data=form.model_dump(mode="json"),
-        )
-        if parser_request is None:
-            raise HTTPInternalServerError(reason="Can't create parser request")
-        parser_request = (
-            await self.parser_request_storage.update_status(
-                id_=parser_request.id,
-                status=RequestStatus.QUEUED,
+        try:
+            form = await self._parse_json(
+                schemas=[ParsePostsVkForm, SimpleVkForm],
             )
-            or parser_request
-        )
-        await self.amqp_master.create_task(
-            form.parser_type,  # type: ignore[attr-defined]
-            kwargs=dict(
-                parser_request_id=parser_request.id,
-                input_data=form.model_dump(),
-            ),
-        )
-        return fast_json_response(data=parser_request.model_dump(mode="json"))
+
+            parser_request = await self.parser_request_storage.create(
+                input_data=form.model_dump(mode="json"),
+            )
+            if parser_request is None:
+                raise HTTPInternalServerError(reason="Can't create parser request")
+            parser_request = (
+                await self.parser_request_storage.update_status(
+                    id_=parser_request.id,
+                    status=RequestStatus.QUEUED,
+                )
+                or parser_request
+            )
+            await self.amqp_master.create_task(
+                form.parser_type,  # type: ignore[attr-defined]
+                kwargs=dict(
+                    parser_request_id=parser_request.id,
+                    input_data=form.model_dump(),
+                ),
+            )
+            return fast_json_response(data=parser_request.model_dump(mode="json"))
+        except TimeoutError as exc:
+            raise HTTPRequestTimeout(reason="Request processing timed out") from exc
