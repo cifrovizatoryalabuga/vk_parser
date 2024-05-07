872309a829a45799a01ae17fc2e187737dd2b520
Yzurgzd <galievr469@gmail.com>
2024-05-07 18:22:56 +0300

    Add models in init

--- vk_parser/db/models/__init__.py
+++ vk_parser/db/models/__init__.py
@@ -4,15 +4,18 @@ from vk_parser.db.models.send_message import SendMessages
 from vk_parser.db.models.vk_group import VkGroup
 from vk_parser.db.models.vk_group_post import VkGroupPost
 from vk_parser.db.models.vk_group_user import VkGroupUser
+from vk_parser.db.models.vk_messages import VkDialogs, VkMessages
 from vk_parser.db.models.vk_user_messanger import Messages, SendAccounts
 
 __all__ = [
+    "AuthUser",
+    "Messages",
     "ParserRequest",
+    "SendAccounts",
+    "SendMessages",
+    "VkDialogs",
+    "VkGroup",
     "VkGroupPost",
     "VkGroupUser",
-    "VkGroup",
-    "SendAccounts",
-    "Messages",
-    "AuthUser",
-    "SendMessages",
+    "VkMessages",
 ]
