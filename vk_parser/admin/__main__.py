ab8c0d1a8eaef8d27f6b5fd8095e33fdda698076
Yzurgzd <galievr469@gmail.com>
2024-05-09 15:33:27 +0300

    Update init Tasks

--- vk_parser/admin/__main__.py
+++ vk_parser/admin/__main__.py
@@ -6,9 +6,7 @@ from aiomisc_log import basic_config
 from vk_parser.admin.arguments import parser
 from vk_parser.admin.deps import config_deps
 from vk_parser.admin.service import Admin
-from vk_parser.tasks.deleting_archive_accounts_task import delete_archive_accounts
-from vk_parser.tasks.deleting_old_send_messages import delete_old_send_messages
-from vk_parser.tasks.resetting_send_accounts_task import reset_send_accounts
+from vk_parser.admin.tasks import Tasks
 from vk_parser.utils.scheduler import SchedulerService
 
 log = logging.getLogger(__name__)
@@ -19,23 +17,30 @@ def main() -> None:
     basic_config(level=args.log_level, log_format=args.log_format)
     config_deps(args)
 
+    tasks = Tasks()
+
     scheduler_service = SchedulerService(
         jobs=[
             {
-                "func": reset_send_accounts,
+                "func": tasks.reset_send_accounts,
                 "trigger": "cron",
                 "hour": 3,
             },
             {
-                "func": delete_archive_accounts,
+                "func": tasks.delete_archive_accounts,
                 "trigger": "cron",
                 "hour": 4,
             },
             {
-                "func": delete_old_send_messages,
+                "func": tasks.delete_old_send_messages,
                 "trigger": "cron",
                 "hour": 5,
                 "day_of_week": "sun",
+            },
+            {
+                "func": tasks.dialogue,
+                "trigger": "interval",
+                "hour": 3,
             },
         ],
     )
